e31b2406356369919cadb338612336c2dd61699d
TamerB <tamer.bhgt@gmail.com>
2017-06-18 08:54:57 +0200

    Added primary code for POST request

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -7,4 +7,20 @@ class PackagesController < ApplicationController
 		@result = Package.get_deps(packages, pack_type, os)
 		render json: @result
 	end
+
+	def create
+		package = params[:package]
+		os = params[:os]
+		pack_type = params[:pack_type]
+		dependencies = params[:dependencies]
+
+		result = Package.new(name: package, pack_type: pack_type)
+		dependencies.each do |dep|
+			result.dependencies.build(name: dep, os: os)
+		end
+
+		result.save!
+
+		render json: result, status: :created
+	end	
 end
